fff25951a9ff69f77e96504bde455e3241adfe4a
jcigama <jcigama77@yahoo.com>
2021-02-11 23:36:01 -0500

    Added all routes, made data storable in sessions through post arrays, displayed data using handlebar notation

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+//Start a session
+session_start();
+
 //Require the autoload file
 require_once('vendor/autoload.php');
 
@@ -24,19 +27,36 @@ $f3->route('GET /personal', function() {
 });
 
 //Profile Page
-$f3->route('GET /profile', function() {
+$f3->route('POST /profile', function() {
+
+    $_SESSION['personal'] = $_POST;
+
     $view = new Template();
     echo $view->render('views/profile.html');
 });
 
 //Interests Page
-$f3->route('GET /interests', function() {
+$f3->route('POST /interests', function() {
+
+    $_SESSION['profile'] = $_POST;
+
     $view = new Template();
     echo $view->render('views/interests.html');
 });
 
 //Summary Page
-$f3->route('GET /summary', function() {
+$f3->route('POST /summary', function() {
+    $_SESSION['interests'] = $_POST;
+    if (isset($_POST) && count($_POST) > 0) {
+
+        $_SESSION['interests'] =  implode(", ", $_SESSION['interests']['interests']);
+    }
+    else {
+        $_SESSION['interests'] =  "none";
+    }
+
+//    var_dump($_SESSION['interests']);
+
     $view = new Template();
     echo $view->render('views/summary.html');
 });
